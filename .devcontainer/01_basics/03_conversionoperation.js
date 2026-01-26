7d2b6258b9ef304fca69b251a18f16c0555523cf
Abhishek Raut <201279255+AbhishekRaut8789@users.noreply.github.com>
2026-01-26 18:47:28 +0000

    conservation of operation

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "Abhisehk"
+
+//console.log(typeof score);
+//console.log(typepf(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false=> 0
+
+let isLoggedIn ="abhishek"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+//"" => false
+//"abhisehk" => true
+
+let someNumber =33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
