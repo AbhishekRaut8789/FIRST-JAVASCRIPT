0632ad16b40decec3d5e593f7e7fd711f1c12f24
Abhishek Raut <201279255+AbhishekRaut8789@users.noreply.github.com>
2026-01-31 14:20:43 +0000

    03.1_conservsionoperation

---
+++ .devcontainer/01_basics/03.1_conversionoperation.js
@@ -0,0 +1,42 @@
+//OPERATOR
+
+let value = 3
+let negValue = -value
+//console.log (negValue);
+
+//console.log(2*2);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2**3); //2^3 =8 
+
+let str1="Hello"
+let str2="Abhishek"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2); // "12"
+//console.log(1 + "2"); // "12"
+//console.log("1" + "2"); // "12"
+//console.log(1 + 2); // 3
+//console.log("1"+2+3); //"123"
+//console.log(1+2+"3"); //"33"
+//console.log( "Hello" + " Abhishek"); //"Hello Abhishek"
+
+//console.log(3+4)*5 % 3 ;
+
+//console.log(+true); //1
+//console.log(+false); //0
+//console.log(true); //true
+//console.log(false); //false
+//console.log(+""); //0//
+
+let num1, num2, num3 
+
+num1, num2, num3 = 2+3
+
+let gamecounter = 100;
+++gamecounter;
+console.log(gamecounter);
